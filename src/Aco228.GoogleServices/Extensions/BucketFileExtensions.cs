127a6ed4a80a0590e9de642053494a081a741267
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-10 22:15:36 +0100

    test ht template

--- src/Aco228.GoogleServices/Extensions/BucketFileExtensions.cs
+++ src/Aco228.GoogleServices/Extensions/BucketFileExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Aco228.Common;
 using Aco228.GoogleServices.Models;
+using Aco228.GoogleServices.Services;
 
 namespace Aco228.GoogleServices.Extensions;
 
@@ -17,6 +18,12 @@ public static class BucketFileExtensions
         var httpClientFactory = ServiceProviderHelper.GetService<IHttpClientFactory>()!;
         var client = httpClientFactory.CreateClient();
         return client.GetByteArrayAsync($"https://storage.googleapis.com/{bucketFile.BucketName}/{bucketFile.FileName}");
+    }
+
+    public static async Task<bool> DeleteAsync(this BucketFile bucketFile)
+    {
+        var bucket = DefaultGoogleBucket.Construct(bucketFile.BucketName);
+        return await bucket.DeleteFile(bucketFile);
     }
     
 }
---
+++ src/Aco228.GoogleServices/Services/DefaultGoogleBucket.cs
@@ -0,0 +1,28 @@
+﻿using Aco228.Common;
+
+namespace Aco228.GoogleServices.Services;
+
+public interface IDefaultGoogleBucket : IGoogleBucket
+{
+    
+}
+
+public class DefaultGoogleBucket : GoogleBucket, IDefaultGoogleBucket
+{
+    private readonly string _bucketName;
+    public override string BucketName => _bucketName;
+    
+    public DefaultGoogleBucket(
+        string bucketName,
+        IGoogleClientProvider googleClientProvider) 
+        : base(googleClientProvider)
+    {
+        _bucketName = bucketName;
+    }
+
+    public static DefaultGoogleBucket Construct(string bucketName)
+    {
+        var googleClientProvider = ServiceProviderHelper.GetService<IGoogleClientProvider>()!;
+        return new DefaultGoogleBucket(bucketName, googleClientProvider);
+    }
+}
